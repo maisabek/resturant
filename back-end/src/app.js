5f8604057826fbc1e18363bcc074b795c79c057e
Mostafa <mismail>
2021-04-12 15:14:42 +0200

    done

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,15 +4,23 @@ const Resturant = require('./router/resturant')
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Accept,Accept-Language,Content-Language,Content-Type')
-    res.header('Access-Control-Expose-Headers', 'Content-Length,Content-Range')
-    next()
-})
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE')
+//     res.header('Access-Control-Allow-Headers', 'Accept,Accept-Language,Content-Language,Content-Type')
+//     res.header('Access-Control-Expose-Headers', 'Content-Length,Content-Range')
+//     next()
+// })
 
 app.use(express.json())
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+
 app.use(Resturant)
 
 module.exports = app
